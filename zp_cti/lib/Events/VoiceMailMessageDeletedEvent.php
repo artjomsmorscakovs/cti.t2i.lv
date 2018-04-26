4ca556648b02545523bbdc83cdfaa2d818cc6c9e
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-04-26 19:03:14 +0300

    endofday

--- zp_cti/lib/Events/VoiceMailMessageDeletedEvent.php
+++ zp_cti/lib/Events/VoiceMailMessageDeletedEvent.php
@@ -20,7 +20,10 @@ public function parseFolderPath($data){
             $datetime = date('Y/m/d'); //when voice mail deleted TODO Maybe put DIRECTORY_SEPARATOR?
             $datefilename = date('Y_m_d_H_i_s');
             
+            
+            //TODO add backward connection to voicemail created event to take data about voicemailboxID?
             $this->folderpath = 'zp_cti'.DIRECTORY_SEPARATOR.'request_log'.DIRECTORY_SEPARATOR.$data->sender.DIRECTORY_SEPARATOR.$datetime.DIRECTORY_SEPARATOR;
+            
             $this->filename = $data->voicemailID.$sep.$data->sender.$sep.$datefilename.$sep.$this->eventType.".xml";   
 }
 
