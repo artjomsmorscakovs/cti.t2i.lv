b1269c076567e473166bef1e68b749a262d45a81
Artjoms Morscakovs <artjomsmorscakovs@gmail.com>
2018-04-26 16:52:41 +0300

    parseFolderPath

--- zp_cti/lib/Events/CallStartedEvent.php
+++ zp_cti/lib/Events/CallStartedEvent.php
@@ -18,19 +18,7 @@ class CallStartedEvent extends Event{
 		
 	}
 	
-	private function parseFolderPath($data){
-		//2018-04-25T13:54:19+0000
-		$sep = "_";
-		
-		$this->datetime = $data->callStarted;
-		$datepath = $this->parseDateTimePath($data->callStarted);
-		$datefilename = $this->parseDateTimeFileName($data->callStarted);
-		
-		$this->folderpath = 'zp_cti'.DIRECTORY_SEPARATOR.'request_log'.DIRECTORY_SEPARATOR.$data->contactID.DIRECTORY_SEPARATOR.$data->direction.DIRECTORY_SEPARATOR.$datepath.DIRECTORY_SEPARATOR;
-		$this->filename = $data->callID.$sep.$data->caller.$sep.$data->destination.$sep.$datefilename.".xml";
-		//zp_cti/request_log/966/in/
-		//1524664459097344_27771153_29822031_datetime.xm;
-	}
+ 
 
 	
 }
