86e23e11354797d32b43c11d8adf0daaeee44f48
Artjoms Morscakovs <artjomsmorscakovs@gmail.com>
2018-04-26 15:51:23 +0300

    dateparse

--- zp_cti/lib/Events/CallStartedEvent.php
+++ zp_cti/lib/Events/CallStartedEvent.php
@@ -5,10 +5,35 @@ class CallStartedEvent{
 	public  $version;
 	public  $data;
 	public  $datetime;
+	public	$folderpath;
 	
 	public function setData($data){
 		$this->data = $data;
+		$this->parseFolderPath($data);
+		
+	}
+	
+	private function parseFolderPath($data){
+		//2018-04-25T13:54:19+0000
+		
 		$this->datetime = $data->callStarted;
+		$datepath = $this->parseDateTime($data->callStarted);
+		
+		$this->folderpath = 'zp_cti'.DIRECTORY_SEPARATOR.'request_log'.DIRECTORY_SEPARATOR.$data->contactID.DIRECTORY_SEPARATOR.$data->direction.DIRECTORY_SEPARATOR.$datepath.DIRECTORY_SEPARATOR;
+		
+		//zp_cti/request_log/966/in/
+	}
+	
+	private function parseDateTime($datetime){
+		$result = '' ;
+		if(!empty($datetime)){
+			$datearr = date_parse($data->callStarted);
+			$result = $datearr['year'].DIRECTORY_SEPARATOR.$datearr['month'].DIRECTORY_SEPARATOR.$datearr['day'];
+		}else {
+			$result = date('Y/m/d');
+		}
+		//return 2018/04/26
+		return $result;
 	}
 }
 
