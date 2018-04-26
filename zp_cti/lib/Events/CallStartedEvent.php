e4f9bc031f3408ef3132d06fb5aa096c09b01197
Artjoms Morscakovs <artjomsmorscakovs@gmail.com>
2018-04-26 16:01:59 +0300

    filename

--- zp_cti/lib/Events/CallStartedEvent.php
+++ zp_cti/lib/Events/CallStartedEvent.php
@@ -6,6 +6,7 @@ class CallStartedEvent{
 	public  $data;
 	public  $datetime;
 	public	$folderpath;
+	public	$filename;
 	
 	public function setData($data){
 		$this->data = $data;
@@ -15,16 +16,24 @@ class CallStartedEvent{
 	
 	private function parseFolderPath($data){
 		//2018-04-25T13:54:19+0000
+		$sep = "_";
 		
 		$this->datetime = $data->callStarted;
-		$datepath = $this->parseDateTime($data->callStarted);
+		$datepath = $this->parseDateTimePath($data->callStarted);
+		$datefilename = $this->parseDateTimeFileName($data->callStarted);
 		
 		$this->folderpath = 'zp_cti'.DIRECTORY_SEPARATOR.'request_log'.DIRECTORY_SEPARATOR.$data->contactID.DIRECTORY_SEPARATOR.$data->direction.DIRECTORY_SEPARATOR.$datepath.DIRECTORY_SEPARATOR;
-		
+		$this->filename = $data->callStarted.$sep.$data->caller.$sep.$data->destination.$sep.$datefilename.".xml";
 		//zp_cti/request_log/966/in/
 	}
 	
-	private function parseDateTime($datetime){
+	private function parseFileName($data){
+		
+		
+		//1524664459097344_27771153_29822031_datetime
+	}
+	
+	private function parseDateTimePath($datetime){
 		$result = '' ;
 		if(!empty($datetime)){
 			$datearr = date_parse($datetime);
@@ -35,6 +44,19 @@ class CallStartedEvent{
 		//return 2018/04/26
 		return $result;
 	}
+	
+	private function parseDateTimeFileName($datetime){
+		$result = '' ;
+		$sep = "_";
+		if(!empty($datetime)){
+			$datearr = date_parse($datetime);
+			$result = $datearr['year'].$sep.$datearr['month'].$sep.$datearr['day'].$sep.$datearr['hour'].$sep.$datearr['minute'].$sep.$datearr['second'];
+		}else {
+			$result = date('Y_m_d_H_i_s');
+		}
+		//return 2018/04/26
+		return $result;
+	}	
 }
 
 ?>
