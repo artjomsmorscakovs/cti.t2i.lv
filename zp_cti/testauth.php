29c10c5475ae7f2579e26398626a5d36315420ba
Artjoms Morscakovs <artjomsmorscakovs@gmail.com>
2018-04-27 13:25:24 +0300

    a

--- zp_cti/testauth.php
+++ zp_cti/testauth.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+/*
 $ch = curl_init();
 $header = array(
    'Content-type: application/vnd.api+json',
@@ -12,10 +13,11 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');;
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-$output = curl_exec($ch);
+$output = curl_exec($ch);*/
 
-print_r($output);
-echo 'ok';
+
+
+
 /*
 $ch = curl_init();
 $header = array(
@@ -36,5 +38,31 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 $output = curl_exec($ch);*/
 
+
+$ch = curl_init();
+$header = array(
+    'Content-type: application/vnd.api+json',
+    'Accept: application/vnd.api+json',
+ );
+$postStr = json_encode(array(
+    'grant_type' => 'password',
+    'client_id' => 'b0eb51cf-bc5d-cd25-c26d-5ae2f6f754e4',
+    'client_secret' => 'client_secret',
+    'username' => 'zptestapi',
+    'password' => 'T2I298220031',
+    'scope' => 'standard:create standard:read standard:update standard:delete standard:delete standard:relationship:create standard:relationship:read standard:relationship:update standard:relationship:delete'
+));
+$url = 'http://crm1.t2i.lv/api/oauth/access_token';
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postStr);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$output = curl_exec($ch);
+
+
+
+print_r($output);
+echo 'ok';
 
 ?>
