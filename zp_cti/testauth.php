f50ea4744a0153073171216b063d6b4fb02cf003
Artjoms Morscakovs <artjomsmorscakovs@gmail.com>
2018-04-27 13:21:32 +0300

    meta/list

--- zp_cti/testauth.php
+++ zp_cti/testauth.php
@@ -2,7 +2,21 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+$ch = curl_init();
+$header = array(
+   'Content-type: application/vnd.api+json',
+   'Accept: application/vnd.api+json',
+);
+$url = 'https://crm1.t2i.lv/api/v8/modules/meta/list';
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');;
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$output = curl_exec($ch);
 
+print_r($output);
+echo 'ok';
+/*
 $ch = curl_init();
 $header = array(
     'Content-type: application/vnd.api+json',
@@ -14,13 +28,13 @@ $postStr = json_encode(array(
     'client_secret' => 'd938225b-3177-5ec7-d356-5adde6e5ee3e',
     'scope' => 'standard:create standard:read standard:update standard:delete standard:delete standard:relationship:create standard:relationship:read standard:relationship:update standard:relationship:delete'
 ));
-$url = 'https://crm1.t2i.lv/lib/API/v8/swagger.json';
+$url = 'https://crm1.t2i.lv/api/oauth2';
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postStr);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-$output = curl_exec($ch);
-print_r($output);
-echo 'ok';
+$output = curl_exec($ch);*/
+
+
 ?>
