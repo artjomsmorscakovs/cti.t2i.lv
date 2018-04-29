3e65fb11b74074b62bbe5bd160f05f6c7198552a
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-04-29 11:43:12 +0300

    Some testing

--- zp_cti/testauth.php
+++ zp_cti/testauth.php
@@ -69,7 +69,7 @@ echo 'ok';*/
 
     $output = $client->retrieveEntry('1234daf730c6-0731-ac81-e553-5ae429e377c2');
 
-    if (isset($output)) {
+    if ($output == false) {
         echo "<h1>OUTPUT IS TRUE</h1>";
     } else {
         echo "<h1>OUTPUT IS FALSE</h1>";
