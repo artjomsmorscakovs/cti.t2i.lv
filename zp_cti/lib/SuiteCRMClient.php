0ace96c04464799fc2e2763181e430a3178e511b
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-05-02 17:53:54 +0300

    testing in progress

--- zp_cti/lib/SuiteCRMClient.php
+++ zp_cti/lib/SuiteCRMClient.php
@@ -19,7 +19,7 @@ class SuiteCRMClient{
 
 	public function createEntry($data){
 	    //POST /api/v8/modules/{module}/{id}
-	    $this->call("v8/modules/t2ilc_t2i_lmt_calls/",json_encode($data), "POST"); //json_encode required here
+	    $this->call("v8/modules/t2ilc_t2i_lmt_calls/",$data, "POST"); //json_encode required here
     }
 
     public function retrieveEntry($id){
@@ -69,7 +69,8 @@ class SuiteCRMClient{
 		curl_setopt($ch, CURLOPT_URL, $this->url.$method);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request);
 		if($request == 'POST'){
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
+            curl_setopt($ch, CURLOPT_URL, $this->url.$method.'/');
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($parameters));
 		}
 		if($request == 'GET'){
 		    //TODO How to add to existing url, not creating a new?
