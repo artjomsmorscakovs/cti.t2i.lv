83195d7a0c65bceca896f063e59ef8d250246dd4
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-05-02 18:58:11 +0300

    testing in progress

--- zp_cti/testauth.php
+++ zp_cti/testauth.php
@@ -70,7 +70,7 @@ echo 'ok';*/
 
 
     $id = $client->findByCall_ID($data['data']['attributes']['callid']);
-    print_r($id['data']);
+    print_r($id['data']->id);
     //$data['data']['id'] = $id->data[0]->id;
     //$client->updateEntry($data);
 
