02bc135b8f680f3d90f1b71e431bccdd7a3d203c
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-05-02 16:31:55 +0300

    testing in progress

--- zp_cti/lib/SuiteCRMClient.php
+++ zp_cti/lib/SuiteCRMClient.php
@@ -42,7 +42,7 @@ class SuiteCRMClient{
 		$this->call('v8/modules/t2ilc_t2i_lmt_calls', json_encode(array()), 'GET');
 	}
 
-    public function findByCall_ID($id=''){
+    public function findByCall_ID($id){
         $output = $this->call('v8/modules/t2ilc_t2i_lmt_calls', $id,'GET');
         foreach ($output->data as $call) {
             $call_id = $call->attributes->callid;
