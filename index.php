ebdd3b78d3abd96da72f312e5a20e49bb3190743
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-05-03 16:06:25 +0300

    testing in progress

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
 	//echo "<h1>Nothing to find here</h1>";
 	$arr = unserialize('a:4:{s:9:"eventType";s:11:"CallStarted";s:7:"version";s:2:"v1";s:4:"type";s:5:"Event";s:4:"data";a:8:{s:6:"status";i:1;s:4:"name";s:4:"test";s:9:"direction";s:3:"out";s:11:"destination";s:8:"29822031";s:6:"callid";i:1524664459097344;s:6:"caller";s:8:"27771153";s:9:"contactid";i:969;s:11:"callstarted";s:24:"2018-04-25T13:54:19+0000";}}');
     print_r($arr);
-
-    $decoded = unserialize('a:4:{s:9:"eventType";s:11:"CallStarted";s:7:"version";s:2:"v1";s:4:"type";s:5:"Event";s:4:"data";a:8:{s:6:"status";i:1;s:4:"name";s:4:"test";s:9:"direction";s:3:"out";s:11:"destination";s:8:"29822031";s:6:"callid";i:1524664459097344;s:6:"caller";s:8:"27771153";s:9:"contactid";i:969;s:11:"callstarted";s:24:"2018-04-25T13:54:19+0000";}}');
+    echo "<br /><br /><br /><br /><br /><br /><br />";
+    $decoded = json_decode('a:4:{s:9:"eventType";s:11:"CallStarted";s:7:"version";s:2:"v1";s:4:"type";s:5:"Event";s:4:"data";a:8:{s:6:"status";i:1;s:4:"name";s:4:"test";s:9:"direction";s:3:"out";s:11:"destination";s:8:"29822031";s:6:"callid";i:1524664459097344;s:6:"caller";s:8:"27771153";s:9:"contactid";i:969;s:11:"callstarted";s:24:"2018-04-25T13:54:19+0000";}}');
+    print_r($decoded);
 
     $mapper = new JsonMapper();
     echo $decoded->eventType;
