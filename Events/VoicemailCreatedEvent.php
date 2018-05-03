7bdc9fb717c6da322ff9cc7320508fc929dad104
Ilya Polanskis <dokolv@gmail.com>
2018-05-03 21:40:48 +0300

    added some notations

--- Events/VoicemailCreatedEvent.php
+++ Events/VoicemailCreatedEvent.php
@@ -14,16 +14,5 @@ class VoicemailCreatedEvent extends Event
         "sender": "23332222",
         "voicemailID": 171118926552
     */
-
-    public function parseFolderPath($data)
-    {
-        //2018-04-25T13:54:19+0000
-        $sep = "_";
-        $datetime = date('Y/m/d'); //when voice mail deleted TODO Maybe put DIRECTORY_SEPARATOR?
-        $datefilename = date('Y_m_d_H_i_s');
-
-        $this->folderpath = 'zp_cti' . DIRECTORY_SEPARATOR . 'request_log' . DIRECTORY_SEPARATOR . $data->sender . DIRECTORY_SEPARATOR . $datetime . DIRECTORY_SEPARATOR;
-        $this->filename = $data->voicemailID . $sep . $data->sender . $sep . $datefilename . $sep . $this->eventType . ".xml";
-    }
 }
 ?>
