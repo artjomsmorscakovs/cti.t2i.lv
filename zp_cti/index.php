89611e68a1a2a7c699fd1cfcec4b7e5d6a1580bf
Ilya Polanskis <Ilya.Polanskis@gmail.com>
2018-05-07 14:18:32 +0300

    Fixing datetime

--- zp_cti/index.php
+++ zp_cti/index.php
@@ -30,7 +30,9 @@ $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) 
 if(strcasecmp($contentType, 'application/json') != 0){
     throw new Exception('Content type must be: application/json');
 }
- 
+
+echo 'BEFORE $content = trim(file_get_contents("php://input")); \n';
+
 //Receive the RAW post data.
 $content = trim(file_get_contents("php://input"));
 
